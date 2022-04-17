f356faf38073274ed006a91e363be56549a9bf33
Marco Seguri <seguri@users.noreply.github.com>
2022-04-17 17:33:58 +0200

    Run prettier

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const iife = document.getElementById("iife")
-const jquery = document.getElementById("jquery")
-const minify = document.getElementById("minify")
-const bookmarkletName = document.getElementById("bookmarkletName")
-const code = document.getElementById("code")
-const outputText = document.getElementById("outputText")
-const link = document.getElementById("link")
+const iife = document.getElementById('iife');
+const jquery = document.getElementById('jquery');
+const minify = document.getElementById('minify');
+const bookmarkletName = document.getElementById('bookmarkletName');
+const code = document.getElementById('code');
+const outputText = document.getElementById('outputText');
+const link = document.getElementById('link');
 
-onBookmarkletNameChange = (event) => {
-  link.textContent = bookmarkletName.value
-}
+onBookmarkletNameChange = event => {
+  link.textContent = bookmarkletName.value;
+};
 
-onCodeChange = (event) => {
-  const minified = UglifyJS.minify(code.value).code
-  outputText.innerHTML = minified
-  link.href = `javascript:void function(){${minified}}()`
-}
+onCodeChange = event => {
+  const minified = UglifyJS.minify(code.value).code;
+  outputText.innerHTML = minified;
+  link.href = `javascript:void function(){${minified}}()`;
+};
 
-bookmarkletName.addEventListener('change', onBookmarkletNameChange)
-code.addEventListener('change', onCodeChange)
+bookmarkletName.addEventListener('change', onBookmarkletNameChange);
+code.addEventListener('change', onCodeChange);
 
-onBookmarkletNameChange()
-onCodeChange()
+onBookmarkletNameChange();
+onCodeChange();
