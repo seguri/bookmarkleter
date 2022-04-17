15de14d37027d06fe0fc556a401775366e161760
Marco Seguri <seguri@users.noreply.github.com>
2022-04-17 17:20:34 +0200

    Manage options to wrap with jQuery or IIFE

--- bookmarkleter.js
+++ bookmarkleter.js
@@ -1,10 +1,28 @@
 const UglifyJS = require('uglify-js');
 
+const jQueryWrap = code => `void function() {
+  function onload($) {${code}};
+  if (!window.jQuery) {
+    const s = document.createElement('script');
+    s.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js';
+    s.onload = function() {onload(jQuery)};
+    document.body.appendChild(s);
+  } else {
+    onload(window.jQuery);
+  }
+}();`;
+
 module.exports = exports = (code, options = {}) => {
   let result = code;
 
+  if (options.jquery || options.jQuery) {
+    result = jQueryWrap(result);
+  } else if (options.iife) {
+    result = `void function() {${result}}();`;
+  }
+
   if (options.minify) {
-    result = UglifyJS.minify(code).code;
+    result = UglifyJS.minify(result).code;
   }
 
   return `javascript:${result}`;
