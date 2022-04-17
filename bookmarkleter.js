438a1830b53e414132852506a63382e6515d3d81
Marco Seguri <seguri@users.noreply.github.com>
2022-04-17 02:45:27 +0200

    Run prettier

--- bookmarkleter.js
+++ bookmarkleter.js
@@ -1,12 +1,11 @@
-const UglifyJS = require('uglify-js')
+const UglifyJS = require('uglify-js');
 
-module.exports = exports = (code, options={}) => {
-  let result = code
+module.exports = exports = (code, options = {}) => {
+  let result = code;
 
   if (options.minify) {
-    result = UglifyJS.minify(code).code
+    result = UglifyJS.minify(code).code;
   }
 
-  return `javascript:${result}`
-}
-
+  return `javascript:${result}`;
+};
