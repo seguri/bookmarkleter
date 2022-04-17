9166f0f0f473179f0291b88e254d90e2c1045af0
Marco Seguri <seguri@users.noreply.github.com>
2022-04-17 17:31:21 +0200

    Update tests

--- test/minify.js
+++ test/minify.js
@@ -3,17 +3,27 @@ const bookmarkleter = require('../bookmarkleter');
 
 describe('minify', () => {
   describe('disabled', () => {
-    it('should not minify the code', () => {
+    it('should not minify a function declaration', () => {
       const code = 'function add(first, second) { return first + second; }';
       const expected =
         'javascript:function add(first, second) { return first + second; }';
       assert.equal(expected, bookmarkleter(code));
     });
+    it('should not minify a log statement', () => {
+      const code = 'console.log($.fn.jquery);';
+      const expected = 'javascript:console.log($.fn.jquery);';
+      assert.equal(expected, bookmarkleter(code));
+    });
   });
   describe('enabled', () => {
-    it('should minify the code', () => {
+    it('should minify a function declaration', () => {
       const code = 'function add(first, second) { return first + second; }';
       const expected = 'javascript:function add(n,d){return n+d}';
+      assert.equal(expected, bookmarkleter(code, {minify: true}));
+    });
+    it('should not minify a log statement', () => {
+      const code = 'console.log($.fn.jquery);';
+      const expected = 'javascript:console.log($.fn.jquery);';
       assert.equal(expected, bookmarkleter(code, {minify: true}));
     });
   });
